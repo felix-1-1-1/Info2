61aa8845c42feb11958f0ac86d61399deabc8915
Felix <felixhz@proton,mail.com>
2021-05-19 10:25:27 +0200

    test

--- src/meddl.java
+++ src/meddl.java
@@ -1,5 +1,5 @@
 public class meddl {
     public static void main(String[] args){
-        System.out.println("Ausgabe aus der main()-Methode");//test2
+        System.out.println("Ausgabe aus der main()-Methode");//github stinkt
     }
 }
