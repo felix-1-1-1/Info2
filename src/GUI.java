4fa6bac484564c01c4cede1ba6fbcd9dd13c0c26
Felix <felixhz@proton,mail.com>
2021-05-19 14:48:48 +0200

    test

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,17 @@
-public class GUI {
+import javax.swing.*;
+
+public class GUI extends JFrame{
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+
+    public static void main(String[] args){
+        new GUI();
+    }
+    public GUI()
+    {
+        setVisible(true);
+        panel1.setVisible(true);
+        button1.setVisible(true);
+    }
 }
