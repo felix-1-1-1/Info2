80c07a0cf1088aef7911f4384f648ed058fa99a9
Felix <felixhz@proton,mail.com>
2021-05-20 23:56:26 +0200

    GUI new

--- src/sql.java
+++ src/sql.java
@@ -7,7 +7,7 @@ public class sql {
         String password = "";
         //System.out.println("Meddl");
         //DriverManager.getConnection(url, user, password);
-        try(Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.0.214:443/altschauerberg", "root", "")){
+        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/informatik projekt", "root", "test")){
             System.out.println("Verbindung");
             String query = "SELECT * FROM noten";
             Statement stmt = conn.createStatement();
