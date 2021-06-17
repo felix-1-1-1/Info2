b562e32edc4387458a70063fcc2550fca920cd2d
Felix <felixhz@proton,mail.com>
2021-06-17 16:49:00 +0200

    Laptop Felix

--- src/NotenGUI.java
+++ src/NotenGUI.java
@@ -51,7 +51,22 @@ public class NotenGUI extends JFrame implements ActionListener{
     public  JTable createTable()
     {
         String[] columnNames = {"Fach", "Notenschnitt Schriftlich","Notenschnitt Mündlich", "Kombiniert"};
-        Object[][] data = {{"Deutsch",sql.getGradesSchriftlich("Deutsch"),sql.getGradesMündlich("Deutsch"), sql.getGradesCombined("Deutsch")},{"dummy","dummy","dummy","dummy"}};
+        Object[][] data = {{"Deutsch",sql.getGradesSchriftlich("Deutsch"),sql.getGradesMündlich("Deutsch"), sql.getGradesCombined("Deutsch")},
+                {"Mathe",sql.getGradesSchriftlich("Mathe"),sql.getGradesMündlich("Mathe"), sql.getGradesCombined("Mathe")},
+                {"Englisch",sql.getGradesSchriftlich("Englisch"),sql.getGradesMündlich("Englisch"), sql.getGradesCombined("Englisch")},
+                {"P-Seminar",sql.getGradesSchriftlich("P-Seminar"),sql.getGradesMündlich("P-Seminar"), sql.getGradesCombined("P-Seminar")},
+                {"W-Seminar",sql.getGradesSchriftlich("W-Seminar"),sql.getGradesMündlich("W-Seminar"), sql.getGradesCombined("W-Seminar")},
+                {"Informatik",sql.getGradesSchriftlich("Informatik"),sql.getGradesMündlich("Informatik"), sql.getGradesCombined("Informatik")},
+
+                {"Physik",sql.getGradesSchriftlich("Physik"),sql.getGradesMündlich("Physik"), sql.getGradesCombined("Physik")},
+                {"Wirtschaft",sql.getGradesSchriftlich("Wirtschaft"),sql.getGradesMündlich("Wirtschaft"), sql.getGradesCombined("Wirtschaft")},
+                {"Geographie",sql.getGradesSchriftlich("Geographie"),sql.getGradesMündlich("Geographie"), sql.getGradesCombined("Geographie")},
+
+                {"Geschichte",sql.getGradesSchriftlich("Geschichte"),sql.getGradesMündlich("Geschichte"), sql.getGradesCombined("Geschichte")},
+                {"Sozialkunde",sql.getGradesSchriftlich("Sozialkunde"),sql.getGradesMündlich("Sozialkunde"), sql.getGradesCombined("Sozialkunde")},
+                {"Französisch",sql.getGradesSchriftlich("Französisch"),sql.getGradesMündlich("Französisch"), sql.getGradesCombined("Französisch")},
+                {"Latein",sql.getGradesSchriftlich("Latein"),sql.getGradesMündlich("Latein"), sql.getGradesCombined("Latein")},
+                {"Russisch",sql.getGradesSchriftlich("Russisch"),sql.getGradesMündlich("Russisch"), sql.getGradesCombined("Russisch")}};
         JTable table = new JTable(data, columnNames);
         table.setFillsViewportHeight(true);
 
