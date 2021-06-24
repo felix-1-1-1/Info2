a516b66b9a05ff5509a781be608644ce7a6f9574
Felix <felixhz@proton,mail.com>
2021-06-24 10:56:15 +0200

    Laptop Felix

--- src/main.java
+++ src/main.java
@@ -1,8 +1,13 @@
+
+import com.formdev.flatlaf.*;
+
 import javax.swing.*;
+
+
 
 public class main {
     public static void main(String[] args)throws ClassNotFoundException, UnsupportedLookAndFeelException, IllegalAccessException, InstantiationException {
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        UIManager.setLookAndFeel(new FlatDarkLaf());
         GUI GUI = new GUI();
         GUI.setVisible(true);
         GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
