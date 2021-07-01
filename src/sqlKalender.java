fd5c90872c6a17ad89dc051d5451f4965071e0a7
Felix <felixhz@proton,mail.com>
2021-07-01 10:06:16 +0200

    Laptop Felix

--- src/sqlKalender.java
+++ src/sqlKalender.java
@@ -22,7 +22,7 @@ String getDate()
     return dtf.format(now);
 }
 
-     Object[][] getDates() {
+    void getDates() {
 
 
 
@@ -41,7 +41,6 @@ String getDate()
 
             while (rs.next()) {
                 for (int i = 1; i <= columns; i++) {
-                    dates += {rs.getInt(i)}
 
                 }
             }
@@ -53,83 +52,14 @@ String getDate()
             System.out.println("fail");
         }
 
-        return round(averageMündl, 2);
-    }
-
-    double getGradesSchriftlich(String Fach) {
-
-
-        double averageSchriftl = 0;
-
-
-        try (Connection conn2 = DriverManager.getConnection(url, user, password)) {
-            conn = conn2;
-            String query = "SELECT Fach, AVG(Punktzahl) FROM noten WHERE Typ = 'Schriftlich' GROUP BY Fach HAVING Fach=" + "'" + Fach + "'";
-            Statement stmt = conn.createStatement();
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            int columns = rs.getMetaData().getColumnCount();
-
-
-            while (rs.next()) {
-                for (int i = 1; i <= columns; i++) {
-                    averageSchriftl = rs.getDouble(2);
-                }
-            }
-            rs.close();
-            stmt.close();
-
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
-            System.out.println("fail");
-        }
-
-        return round(averageSchriftl,2);
-    }
-
-    double getGradesAll() {
-
-
-        double avgAll = 0;
-
-
-        try (Connection conn2 = DriverManager.getConnection(url, user, password)) {
-            conn = conn2;
-            String query = "select avg(mw) from\n" +
-                    "(SELECT avg(Punktzahl) as MW FROM `noten` WHERE typ = \"Mündlich\"\n" +
-                    "union ALL\n" +
-                    "SELECT  avg(Punktzahl) as MW FROM `noten` WHERE typ = \"Schriftlich\") as Untertab1";
-            Statement stmt = conn.createStatement();
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            int columns = rs.getMetaData().getColumnCount();
-
-
-            while (rs.next()) {
-                for (int i = 1; i <= columns; i++) {
-                    avgAll = rs.getDouble(1);
-                }
-            }
-            rs.close();
-            stmt.close();
-
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
-            System.out.println("fail");
-        }
-
-        return round(avgAll,2);
     }
 
 
 
-    double getGradesCombined(String Fach) {
-        double averageCombined;
 
-        averageCombined = (getGradesSchriftlich(Fach)+getGradesMündlich(Fach))/2;
 
-        return round(averageCombined,2);
-    }
+
+
 
     void addGrade(String Fach, int Punktzahl, String Typ)
     {
@@ -151,12 +81,6 @@ String getDate()
         }
     }
 
-
-    double round(double number, int digits)
-    {
-        double result = Math.round(number*Math.pow(10,digits))/Math.pow(10, digits);
-        return result;
-    }
 
 
     /**
